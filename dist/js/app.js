dd8dc0fa66c7eba7f7ee812d3ccba55fc3046ebd
ADzhyhan <a.dzhyhan@gmail.com>
2021-10-18 15:10:08 +0300

    add animations to the project

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector('.navigation__btn');
 const navList = document.querySelector('.navigation__list'); 
+// const AOS = require('aos');
+
+AOS.init();
 
 menuBtn.addEventListener('click', (e) => {
   navList.classList.toggle('navigation__list--active');
--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,25 @@ function style() {
     .pipe(browserSync.stream());
 }
 
+function libscss() {
+  return gulp.src([
+    './node_modules/aos/dist/aos.css',
+  ])
+    .pipe(concat('_libs.scss'))
+    .pipe(gulp.dest('src/scss'))
+    .pipe(browserSync.reload({stream: true}))
+}
+
+function libsjs() {
+  return gulp.src([
+    './node_modules/aos/dist/aos.js',
+  ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.reload({stream: true}))
+}
+
 function html() {
   return gulp.src('./src/*.html')
     .pipe(gulp.dest('dist/'))
@@ -49,6 +68,8 @@ async function build() {
   await html();
   await font();
   await script();
+  await libscss();
+  await libsjs();
 }
 
 function watch() {
@@ -70,6 +91,8 @@ exports.style = style;
 exports.html = html;
 exports.font = font;
 exports.image = image;
+exports.libscss = libscss;
+exports.libsjs = libsjs;
 exports.script = script;
 exports.build = build;
 exports.watch = watch;
